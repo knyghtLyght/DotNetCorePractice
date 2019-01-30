ec5ae3894ab8729417b4643c67a67269f5df91ec
knyghtlyght <kevnaf@gmail.com>
2019-01-30 15:22:53 -0800

    mvc moduel

---
+++ DutchTreat/DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,25 @@
+﻿//To prevent leaking into GLobal scope we wrap the JS
+//in an anon function with jquery .ready
+$(document).ready(() => {
+    console.log("Hello test")
+
+    var theForm = $("#theForm")
+    theForm.hide();
+
+    var button = $("#buyButton");
+    button.on("click", () => {
+        console.log("Buy item clicked")
+    });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.toggle();
+    });
+});
