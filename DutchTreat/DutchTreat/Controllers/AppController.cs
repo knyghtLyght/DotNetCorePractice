8263bf5cc8dc50c43c1771cf5656a45055bce09a
knyghtlyght <kevnaf@gmail.com>
2019-02-04 10:19:10 -0800

    Bootstrap Module

--- DutchTreat/DutchTreat/Controllers/AppController.cs
+++ DutchTreat/DutchTreat/Controllers/AppController.cs
@@ -1,4 +1,5 @@
-﻿using DutchTreat.ViewModels;
+﻿using DutchTreat.Services;
+using DutchTreat.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,6 +10,13 @@ namespace DutchTreat.Controllers
 {
     public class AppController : Controller
     {
+        private readonly IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
         public IActionResult Index()
         {
 
@@ -30,11 +38,9 @@ namespace DutchTreat.Controllers
             if (ModelState.IsValid)
             {
                 // Send the email
-                
-            }
-            else
-            {
-                //show the errors
+                _mailService.SendMessage("kevnaf@gmail.com", model.Subject, $"From: {model.Email}, Message: {model.Name} - {model.Message}");
+                ViewBag.UserMessage = "Mail Sent";
+                ModelState.Clear();
             }
             return View();
         }
